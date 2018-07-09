aa48ccd1fffb14e788b9a24c925aedf9f5c3c506
Javi Fontan <jfontan@gmail.com>
2018-07-09 16:50:33 +0200

    Add simple executable

--- src/ghnews.cr
+++ src/ghnews.cr
@@ -1,6 +1,16 @@
 require "./ghnews/*"
+require "./github/*"
 
-# TODO: Write documentation for `Ghnews`
-module Ghnews
-  # TODO: Put your code here
+def panic(string)
+  STDERR.puts(string)
+  exit(1)
 end
+
+token = ENV["GITHUB_TOKEN"]?
+
+panic("no GITHUB_TOKEN set") if !token
+
+g = GitHub::Client.new(token)
+n = g.notifications
+
+pp n
